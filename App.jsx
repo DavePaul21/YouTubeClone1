8b741b61138abe788a015649c545d2f7cc95caed
Dave G <71780214+DavePaul21@users.noreply.github.com>
2023-02-06 18:12:12 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Header from './components/header'
+
+
+function App() {
+  
+
+  return (
+  <div>
+    <Header />
+  </div>  
+  )
+}
+
+export default App
